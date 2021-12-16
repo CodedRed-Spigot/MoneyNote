d1741c4631fb882ae55dcd4586368498abe2dabd
CodedRed <devcodedred@gmail.com>
2021-12-15 20:49:16 -0600

    Test Commit

---
+++ src/main/java/dev/codedred/moneynote/MoneyNote.java
@@ -0,0 +1,17 @@
+package dev.codedred.moneynote;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MoneyNote extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
